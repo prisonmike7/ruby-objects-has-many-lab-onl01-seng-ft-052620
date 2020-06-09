4830c05470e9c1008a98fc7a793f2afd36acfe83
Michael Harris <mwharris7@gmail.com>
2020-06-09 13:39:30 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,28 @@
-class Author
+class Artist
 
   attr_accessor :name
 
-  # def initialize(name)
-  #   @name = name
-  # end
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def songs
+    @songs
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def add_song_by_name(name, genre)
+    song = Song.new(name, genre)
+    add_song(song)
+  end
 
 end
